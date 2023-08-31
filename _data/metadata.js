bb8fcac870e5da25ea519a95a1ec322f566240ee
Steelaxel <136767533+Steelaxel@users.noreply.github.com>
2023-08-31 21:30:10 +0100

    change to title
    Change in title

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "Steelaxel blog",
+	title: "Steelaxel first blog",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
